ac325f45408d9f715696225ad3db6283e560537b
sven <godockly@gmail.com>
2021-03-11 22:34:55 +0100

    enhancements

--- bump.go
+++ bump.go
@@ -1,66 +1,108 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "flag"
-  "io"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/logrusorgru/aurora"
+
+	"github.com/thisisdevelopment/go-dockly/xerrors/iferr"
 )
 
 const (
-  filename = "VERSION"
-  format = "%d.%d.%d%s\n"
+	filename = "VERSION"
+	format   = "%d.%d.%d%s"
 )
 
+// bump is a little helper to update a semantic VERSION file
 func main() {
+	var err error
+	var f *os.File
+	var current, hash string
+	var major, minor, patch int
+	var sections = map[string]*int{"major": &major, "M": &major, "minor": &minor, "m": &minor, "patch": &patch, "p": &patch}
 
-  var force = flag.Bool("f", false, "force create " + filename)
-  var section = flag.String("b", "patch", "which section to bump: major or M, minor or m, patch or p")
-  var commit = flag.String("c", "", "commit hash")
-  var setman = flag.String("s", "", "set manually (overrides -b -c): M[.m[.p[-hash]]]")
-  var major, minor, patch int
-  var sections = map[string]*int{"major": &major, "M": &major, "minor": &minor, "m": &minor, "patch": &patch, "p": &patch}
-  var err error
-  var f *os.File
-  var hash string
+	var force = flag.Bool("f", false, "force create "+filename)
+	var section = flag.String("b", "patch", "which section to bump: major or M, minor or m, patch or p")
+	var commit = flag.Bool("c", false, "git commit hash")
+	var manual = flag.String("s", "", "set manual (overrides -b -c): M[.m[.p[-hash]]]")
+	flag.Parse()
 
-  flag.Parse()
+	bumpme, ok := sections[*section]
+	if !ok {
+		iferr.Exit(fmt.Errorf("invalid bump: %s", *section))
+	}
 
-  bumpme, valid := sections[*section]
-  if ! valid {
-    fmt.Fprintf(os.Stderr, "invalid bump: %s\n", *section)
-    os.Exit(1)
-  }
+	if *force {
+		f, err = os.Create(filename)
+		iferr.Exit(err, "failed to create VERSION")
+	} else {
+		f, err = os.OpenFile(filename, os.O_RDWR, 0644)
+		iferr.Exit(err, "consider using: bump -f")
+	}
 
-  if *force {
-    f, err = os.Create(filename)
-  } else {
-    f, err = os.OpenFile(filename, os.O_RDWR, 0644)
-  }
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-    os.Exit(1)
-  }
+	defer func() {
+		iferr.Exit(f.Close(), "closing VERSION")
+	}()
 
-  defer f.Close()
+	if *manual != "" {
+		// read semantic from manual argument
+		fmt.Sscanf(*manual, format, &major, &minor, &patch, &hash)
+	} else {
+		// read semantic from file
+		fmt.Fscanf(f, format, &major, &minor, &patch, &hash)
+		current = fmt.Sprintf(
+			format,
+			aurora.Red(major),
+			aurora.BrightGreen(minor),
+			aurora.Cyan(patch),
+			aurora.Yellow(hash),
+		)
+		*bumpme++
 
-  if *setman != "" {
-    fmt.Sscanf(*setman, format, &major, &minor, &patch, &hash)
-  } else {
-    fmt.Fscanf(f, format, &major, &minor, &patch, &hash)
-    *bumpme++
+		if *commit {
+			// append commit hash
+			hash = "-" + getCommitHash("HEAD")
+		} else {
+			hash = "" // can be set previously
+		}
+	}
 
-    if *commit != "" {
-      hash = "-" + *commit
-    }
-  }
+	_, err = f.Seek(0, io.SeekStart)
+	iferr.Exit(err, "io seek")
 
-  _, err = f.Seek(0,io.SeekStart)
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-    os.Exit(1)
-  }
+	_, err = fmt.Fprintf(f, format, major, minor, patch, hash)
+	iferr.Exit(err, "io write")
 
-  fmt.Printf(format, major, minor, patch, hash)
-  fmt.Fprintf(f, format, major, minor, patch, hash)
+	fmt.Printf(
+		"version %s bumped to "+format+"\n",
+		current,
+		aurora.Red(major),
+		aurora.BrightGreen(minor),
+		aurora.Cyan(patch),
+		aurora.Yellow(hash),
+	)
+}
+
+func getCommitHash(path string) string {
+	f, err := os.OpenFile(".git/"+path, os.O_RDONLY, 0644)
+	iferr.Exit(err, "failed to extract commit hash")
+
+	defer func() {
+		iferr.Exit(f.Close(), fmt.Sprintf("closing %s", path))
+	}()
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Scan()
+	var row = scanner.Text()
+	if row[0:5] == "ref: " {
+		// ref: refs/heads/master
+		return getCommitHash(row[5:]) // refs/heads/master
+	}
+
+	return row[:7] // 4fa39df or short of revision hash (4fa39dfe2e8be5838fc4251f6aada4caa59ea2bf)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thisisdevelopment/bump
+
+go 1.16
+
+require github.com/thisisdevelopment/go-dockly v1.0.2
