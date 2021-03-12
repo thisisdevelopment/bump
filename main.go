c38d4cc62b872d9b89b8b3511d605e6e3370c984
sven <godockly@gmail.com>
2021-03-12 10:59:44 +0100

    split up into sub-pkgs

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 
 	"github.com/logrusorgru/aurora"
 	log "github.com/sirupsen/logrus"
 	"github.com/thisisdevelopment/bump/pkg/version"
+	"github.com/thisisdevelopment/bump/pkg/xerr"
+	"github.com/thisisdevelopment/bump/pkg/xgit"
+	"github.com/thisisdevelopment/bump/pkg/xio"
 )
 
 const (
-	filename = "VERSION"
+	path = "VERSION"
 )
 
 // bump is a little helper to update a semantic VERSION file
@@ -23,77 +24,35 @@ func main() {
 	var vNew *version.Version
 	var vCurrent, hash string
 	var section = flag.String("b", "patch", "which section to bump: major, minor or patch")
-	var force = flag.Bool("f", false, "force create ./"+filename)
+	var force = flag.Bool("f", false, "force create ./"+path)
 	var commit = flag.Bool("c", false, "append git commit hash")
 	flag.Parse()
 
-	if *force {
-		f, err = os.Create(filename)
-		exitif(err, "failed to create %s", filename)
-	} else {
-		f, err = os.OpenFile(filename, os.O_RDWR, 0644)
-		exitif(err, "consider using: bump -f")
-	}
+	f, err = xio.GetFile(path, *force)
+	xerr.Exitif(err, "failed to create %s", path)
 
 	defer func() {
-		exitif(f.Close(), "failed to close %s", filename)
+		xerr.Exitif(f.Close(), "failed to close %s", path)
 	}()
 
-	vCurrent = read(f)
-	var vType = version.Type(*section)
-	vNew, err = version.Change(vType, vCurrent)
-	exitif(err, "failed to change version")
+	vCurrent = xio.ReadFirstRow(f)
+	vNew, err = version.Change(version.Type(*section), vCurrent)
+	xerr.Exitif(err, "failed to change version")
 
 	if *commit {
 		// append commit hash
-		hash = "-" + getCommitHash("HEAD")
+		hash = "-" + xgit.GetCommitHash("HEAD")
 	} else {
 		hash = "" // clear if set previously
 	}
 
-	_, err = f.Seek(0, io.SeekStart)
-	exitif(err, "io seek failed")
+	err = xio.ReplaceContent(f, fmt.Sprintf("%s%s", vNew.String(), hash))
+	xerr.Exitif(err, "failed to replace content")
 
-	_, err = fmt.Fprintf(f, "%s%s", vNew.String(), hash)
-	exitif(err, "io write failed")
-
-	fmt.Printf(
+	log.Printf(
 		"version %s bumped to %s%s\n",
 		aurora.Cyan(vCurrent),
 		aurora.BrightGreen(vNew.String()),
 		aurora.Yellow(hash),
 	)
-}
-
-func getCommitHash(path string) string {
-	f, err := os.OpenFile(".git/"+path, os.O_RDONLY, 0644)
-	exitif(err, "failed to extract commit hash")
-
-	defer func() {
-		exitif(f.Close(), "failed to close %s", path)
-	}()
-
-	var row = read(f)
-	if row[0:5] == "ref: " {
-		// ref: refs/heads/master
-		return getCommitHash(row[5:]) // refs/heads/master
-	}
-
-	return row[:7] // 4fa39df or short of revision hash (4fa39dfe2e8be5838fc4251f6aada4caa59ea2bf)
-}
-
-func read(f *os.File) string {
-	scanner := bufio.NewScanner(f)
-	scanner.Scan()
-	return scanner.Text()
-}
-
-func exitif(err error, format string, ctx ...interface{}) {
-	if err != nil {
-		if len(ctx) > 0 {
-			format = aurora.Sprintf(format, ctx...)
-		}
-		log.Error(aurora.Sprintf("%v %s", aurora.BrightRed(err), aurora.Yellow(format)))
-		os.Exit(-1)
-	}
 }
